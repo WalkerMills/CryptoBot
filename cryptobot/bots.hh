c48b3a4586503bb3df807c4e0f77fffba3682f68
alex lew <alex@localhost.localdomain>
2014-01-25 10:10:24 -0800

    Implements more bot functionality
    Classes/interface is still incomplete, and untested

--- cryptobot/bots.hh
+++ cryptobot/bots.hh
@@ -18,6 +18,20 @@ func_map["TA_EMA"] = &TA_EMA;
 func_map["TA_MACD"] = &TA_MACD;
 func_map["SMA"] = &TA_SMA;
 
+enum str AlgoRuleType {ConstComp, VarComp, Exist};
+
+class DimensionError: public exception {
+    virtual const char* what() const throw() {
+        return "The dimensions are mismatched.";
+    }
+} DimensionError;
+
+class RuleTypeError: public exception {
+    virtual const char* what() const throw() {
+        return "The algo rule type does not match the arguments"
+    }
+} TypeError;
+
 class TaInd {
 private:
     char *function;
@@ -32,7 +46,6 @@ public:
     TaInd (char *function, int start, int end, const double *in,
            int optInTimePeriod, int optInMAType);
 
-    void Parse (char filename, int start, int stop); 
 };
 
 class AlgoBot {
@@ -43,15 +56,79 @@ class AlgoBot {
     vector<TaInd *> taQueue;
     
     boolean update() {
-        for (TaInd * : taQueue) {
+        for (TaInd *indicator : taQueue) {
 
         } 
-    };
+    }
+
+    vector<int> crossover(int indicator1[], int indicator2[]) {
+        
+        if (indicator1.size() != indicator2.size()) {
+            throw DimensionError;
+        }
+
+        vector<int> crossovers;
+        int size = indicator1.size();
+        int difference = indicator1[0] - indicator2[0];
+        int sign;
+
+        if (difference > 0) {
+            sign = 1;
+        }
+        else {
+            sign = -1;
+        }
+
+        while(i = 1; i++; i < size) {
+            difference = indicator1[i] - indicator2[i];
+            if ((difference > 0 and sign == -1) or (difference < 0 and sign == 1)) {
+                crossovers.insert(i);
+                sign = -sign;
+            }
+        }
+        return crossovers;
+    }
+
+    boolean existence(TaInd *indicator) {
+
+    }
+
 };
 
 AlgoBot::AlgoBot (char botname) {
     AlgoName = botname;
 }
+
+class AlgoRule (TaInd indicator1, TaInd indicator2 = NULL, AlgoRuleType type, constant = NULL) {
+    if (indicator2 != NULL and AlgoRuleType != 'VarComp') {
+        throw TypeError;
+    }
+
+    if (AlgoRuleType = 'ConstComp' and constant == NULL) {
+        throw TypeError;
+    }
+
+    if (indicator2 == NULL and AlgoRuleType == 'VarComp') {
+        throw TypeError;
+    }
+
+
+    TaInd indicator1 = indicator1;
+    TaInd indicator2 = indicator2;
+    AlgoRuleType = type;
+    constant = constant;
+
+    public: 
+
+    AlgoRule ()
+
+
+}
+
+
+
+
+
 
 class User {
     public: 
