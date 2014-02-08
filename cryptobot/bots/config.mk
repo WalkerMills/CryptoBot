2af6316449efe42d5ac22bbc04ee71832a8c435b
Walker Mills <walker.mills7@gmail.com>
2014-02-07 17:41:32 -0800

    Removes Cython interface
    Cython was creating modules that hung indefinitely (and unbreakably) on
    import; it was not working, and in general has somewhat poor C++
    support, so I'm transitioning to boost::python

--- cryptobot/bots/config.mk
+++ cryptobot/bots/config.mk
@@ -4,12 +4,6 @@ SHELL = /bin/sh
 # Default C++ compiler
 CXX = g++
 
-# Default distutils compiler
-DISTUTILS = python2 setup.py build_ext -i
-
-# Default Cython compiler
-CYTHON = cython
-
 # Directory containing source files
 srcdir = $(CURDIR)
 
@@ -23,7 +17,7 @@ libdir = $(DESTDIR)$(srcdir)/lib
 bindir = $(DESTDIR)$(srcdir)/bin
 
 # Default compiler flags for C
-CFLAGS = -Og -g -march=native -mtune=generic -pipe -fstack-protector \
+CFLAGS = -O3 -march=native -mtune=generic -pipe -fstack-protector \
 --param=ssp-buffer-size=4 -Wno-sign-compare -Wno-write-strings \
 -Wno-unused-function
 
@@ -33,27 +27,13 @@ CXXFLAGS = -std=c++11
 # Default linker flags
 LD_FLAGS = -L$(libdir)
 
-# Default Cython flags
-CYTHON_FLAGS = -2 --cplus
-
-# Default distutils flags
-DISTUTILS_FLAGS = --build-lib $(libdir) --include-dirs $(includedir) \
---cython-cplus -rpath
-
 # Compiler flags for compiling with TA-Lib
 TA_CFLAGS = -I/usr/include/ta-lib
 TA_LDFLAGS = -lta_lib
-TA_ALL = $(TA_CFLAGS) $(TA_LDFLAGS)
 
 # Compiler flags for compiling with MySQL++
 MYSQLPP_CFLAGS = -I/usr/include/mysql -I/usr/include/mysql++ 
 MYSQLPP_LDFLAGS = -lmysqlpp -lmysqlclient
-MYSQLPP_ALL = $(MYSQLPP_CFLAGS) $(MYSQLPP_LDFLAGS)
-
-# Compiler flags for compiling with Cython
-CYTHON_CFLAGS = -pthread -fwrapv -fno-strict-aliasing -I/usr/include/python2.7 
-CYTHON_LDFLAGS = -lpython2.7
-CYTHON_ALL = $(CYTHON_CFLAGS) $(CYTHON_LDFLAGS)
 
 # All C flags required for normal compilation
 ALL_CFLAGS = -Wall $(CFLAGS) $(CXXFLAGS) -I$(includedir) -L$(libdir)
