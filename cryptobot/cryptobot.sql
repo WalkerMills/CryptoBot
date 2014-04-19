1e59e82940e80686b22a59a1587d5a0136778346
Walker Mills <walker.mills7@gmail.com>
2014-04-19 11:11:12 +0000

    Finished migrating schemas to NuoDB

--- cryptobot/cryptobot.sql
+++ cryptobot/cryptobot.sql
@@ -7,16 +7,16 @@ DROP TABLE IF EXISTS cryptobot_host;
 DROP TABLE IF EXISTS cryptobot_trade;
 
 CREATE TABLE cryptobot_key(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(24) NOT NULL,
-    key CHAR(36) NOT NULL,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name STRING NOT NULL,
+    hash VARCHAR(36) NOT NULL,
     ciphertext BINARY(48) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE cryptobot_uses(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     uid INT NOT NULL,
     kid INT NOT NULL,
 
@@ -27,14 +27,14 @@ CREATE TABLE cryptobot_uses(
 );
 
 CREATE TABLE cryptobot_bot(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(24) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE cryptobot_owns(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     uid INT NOT NULL,
     bid INT NOT NULL,
 
@@ -45,29 +45,29 @@ CREATE TABLE cryptobot_owns(
 );
 
 CREATE TABLE cryptobot_host(
-    id INT NOT NULL AUTO_INCREMENT,
-    ip INT UNSIGNED NOT NULL,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    addr INT NOT NULL,
     workload INT NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE cryptobot_runs(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     hid INT NOT NULL,
     bid INT NOT NULL,
 
     PRIMARY KEY (id),
     UNIQUE (hid, bid),
     FOREIGN KEY (hid) REFERENCES cryptobot_host(id),
-    FOREIGN KEY (bid) REFERENCES cryptobot_bot(id),
+    FOREIGN KEY (bid) REFERENCES cryptobot_bot(id)
 );
 
 CREATE TABLE cryptobot_trade(
-    id INT NOT NULL AUTO_INCREMENT,
-    time INT(10) NOT NULL,
-    price NUMERIC(17, 12) NOT NULL,
-    amount NUMERIC(20, 12) NOT NULL,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    time INT NOT NULL,
+    price DOUBLE NOT NULL,
+    amount DOUBLE NOT NULL,
 
     PRIMARY KEY (id)
 );
